28258a29c9ff0969499a33e05f48ac1994bbf6a3
MrSOA <Mrsoa1312@gmail.com>
2023-02-04 23:25:35 -0500

    Final project

--- client1.py
+++ client1.py
@@ -34,8 +34,8 @@ def server():
     PORT = int(PORT)
     #IP = socket.gethostbyname(socket.gethostname())
     #PORT = 5005
-    addr = (IP, PORT)
-    return addr
+    address = (IP, PORT)
+    return address
 
 #ADDR = server()
 SIZE = 1024
@@ -46,12 +46,16 @@ FORMAT = "utf-8"
 
 
 
-def ls(client, path):
+def ls(client, path,currentPath):
     if not path or path.isspace():
-        path = "."
-        print(path)
-    client.send(f"ls {path}".encode(FORMAT))
+        ppath = "."
+    else:
+        ppath=path
+#        print(path)
+#    cd(client,path,currentPath)
+    client.send(f"ls {ppath} {currentPath}".encode(FORMAT))
     msg = client.recv(SIZE).decode(FORMAT)
+ #   cpath = client.recv(SIZE).decode(FORMAT)
     print(msg)
 
 def cd(client, newCurrentPath,currentPath):
@@ -71,12 +75,33 @@ def cd(client, newCurrentPath,currentPath):
             return
             
     else:"""
+    '''
     client.send(f"cd {newCurrentPath}".encode(FORMAT))
     if client.recv(SIZE).decode(FORMAT) == "ok":
         return f"{newCurrentPath}"
+    '''
+    '''
+    client.send(f"cd {newCurrentPath} {currentPath}".encode(FORMAT))
+    if client.recv(SIZE).decode(FORMAT) == "ok":
+        return f"{currentPath}/{newCurrentPath}"
     else:
         print("Directory not found")
         return 
+    '''
+    if newCurrentPath[:2] == "..":
+        if currentPath == "/home/mrsoa":
+            print("You are already in root")
+            return currentPath
+        else:
+            currentPath = currentPath[:currentPath.rfind("/")]
+            return currentPath
+    else:
+        client.send(f"cd {newCurrentPath} {currentPath}".encode(FORMAT))
+        if client.recv(SIZE).decode(FORMAT) == "ok":
+            return f"{currentPath}/{newCurrentPath}"
+        else:
+            print("Directory not found")
+            return currentPath
 
 def mkdir(client, newDir):
     client.send(f"mkdir {newDir}".encode(FORMAT))
@@ -85,38 +110,38 @@ def mkdir(client, newDir):
     else:
         print("Directory already exists")
 
-def upload(client, name,path): #don't know if file should be a path or just the name for now it's just the name
-    files = os.listdir(path)
-    if name in files:
-        client.send(f"upload {name} {path}".encode(FORMAT))
-    
-        #open file
-        file = open(name , "rb")
+def upload(client, name, path): #don't know if file should be a path or just the name for now it's just the name
+#    cpath = os.getcwd
+ #   files = os.listdir(cpath)
+  #  if name in files:
+    client.send(f"upload {name} {path}".encode(FORMAT))
 
-        #send file name
-        if client.recv(SIZE).decode(FORMAT) == "not ok":
-            print("File name already exists")
-            file.close()
-            return
-        
-        while True:
-            read = file.read(SIZE)
-            if not read:
-                break
-            client.send(read)
-        
-        file.close
-        print("File sent")
-    else : 
-        print(f"There is no {name} file found")
+    #open file
+    file = open(name , "rb")
+
+    #send file name
+    if client.recv(SIZE).decode(FORMAT) == "not ok":
+        print("File name already exists")
+        file.close()
+        return
+    
+    while True:
+        read = file.read(SIZE)
+        if not read:
+            break
+        client.send(read)
+    
+    file.close
+    print("File sent")
+#    else : 
+#       print(f"There is no {name} file found")
 
 def download(client, name, path):
     client.send(f"download {name} {path}".encode(FORMAT))
 
     if client.recv(SIZE).decode(FORMAT) == "not ok":
         print("File does not exist")
-        return
-    
+        return    
     file = open(name, "wb")
     while True :
         write = client.recv(SIZE)
@@ -125,40 +150,34 @@ def download(client, name, path):
             break
     file.close()
 
-
 def client_program():
     try:
         client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client.connect(server())
-        currentDir = os.getcwd()
-        path = "."
-
+        currentDir = "/home/mrsoa"
+#        path = ".."
         while True:
             msg = input("-> ")
             if msg == "exit":
                 client.send(msg.encode(FORMAT))
                 print(client.recv(SIZE).decode(FORMAT))
                 break
-
             elif msg[:2] == "ls":
-                ls(client, msg[3:])
-
+                ls(client, msg[3:],currentDir)
             elif msg[:3] == "cd ":
-                cd(client, msg[3:], currentDir)
+                currentDir=cd(client, msg[3:], currentDir)
                 print(currentDir)
-
             elif msg[0:6] == "mkdir ":
                 print("mkdir")
                 mkdir(client, msg[6:])
-
             elif msg[0:7] == "upload ":#need to verify if the file exists
                 print("uploading...")
-                upload(client, msg[7:], path)
+                upload(client, msg[7:], currentDir)
             elif msg[0:9] == "download ":
                 print("downloading...")
                 #send download command to the server with as a third argument the current directory
                 #send the file name to the server
-                download(client, msg[9:], path)
+                download(client, msg[9:], currentDir)
                 #receive an ok message from the server or an error message
                 #receive the file from the server
                 #save the file in the client
--- server1.py
+++ server1.py
@@ -3,6 +3,7 @@ import socket
 import sys
 import threading
 import re
+import datetime
 
 
 # Vérifie si l'adresse IP entrée est valide
@@ -18,6 +19,7 @@ def is_valid_port(PORT):
     return match != None
 
 # Exemple d'utilisation
+'''
 SERVER  = input("Entrez l'adresse IP: ")
 if not is_valid_ip(SERVER ):
     sys.exit("Adresse IP non valide")
@@ -28,10 +30,29 @@ if not is_valid_port(PORT):
 
 IP = SERVER
 PORT = int(PORT)
-
+'''
+def server():
+    while True :
+        SERVER  = input("Enter the IP Address of the server: ")
+        if not is_valid_ip(SERVER):
+            print("IP address is not valide, please try again")
+        else:
+            break
+    while True: 
+        PORT = input("Enter the Port of the server: ")
+        if not is_valid_port(PORT):
+            print("Port is not valide, please try again")
+        else:
+            break
+    IP = SERVER
+    PORT = int(PORT)
+    #IP = socket.gethostbyname(socket.gethostname())
+    #PORT = 5005
+    addr = (IP, PORT)
+    return addr
 #IP = socket.gethostbyname(socket.gethostname())
 #PORT = 5005
-ADDR = (IP, PORT)
+#ADDR = (IP, PORT)
 SIZE = 1024
 FORMAT = "utf-8"
 
@@ -42,20 +63,26 @@ def send_file(conn, file):
     pass
 
 
-def ls(conn, path):
+def ls(conn, path, current_path):
     directories = ""
+#    current_path = os.getcwd()
     try:
-        files = os.listdir(path)
+ #       os.chdir(current_path)
+        files = os.listdir(f"{current_path}/{path}")
         for name in files:
             directories += f"\n{name}"
         if directories == "":
             directories = " This directory is empty"
+ #       conn.send(current_path.encode(FORMAT))
+
         conn.send(directories[1:].encode(FORMAT))
+
     except:
         directories = " directory not found"
         conn.send(directories[1:].encode(FORMAT))
 
 def cd(conn, newPath,currentPath):
+    '''
 #    nPath=f"{newPath}"
     print(newPath)
     nPath=str(newPath)
@@ -65,6 +92,14 @@ def cd(conn, newPath,currentPath):
     try:
         conn.send("ok".encode(FORMAT))
         os.chdir(nPath)
+    except:
+        conn.send("not ok".encode(FORMAT))
+    '''
+    files = os.listdir(currentPath)
+    try:
+ #       if str(newPath) in files:
+        conn.send("ok".encode(FORMAT))
+        os.chdir(f"{currentPath}/{newPath}")
     except:
         conn.send("not ok".encode(FORMAT))
         
@@ -87,8 +122,8 @@ def receive_file(conn, name, path):
     if name in files:
         conn.send("not ok".encode(FORMAT))
         return
-    else:
-        conn.send("ok".encode(FORMAT))
+    
+    conn.send("ok".encode(FORMAT))
     file = open(new_path, "wb") #openeing a non existing file will create it
     while True :
         write = conn.recv(SIZE)
@@ -96,7 +131,7 @@ def receive_file(conn, name, path):
         if sys.getsizeof(write) < 1024: #if the size of the data is less than 1024 bytes it means that it is the last data
             break
     file.close()
-    #conn.send("ok".encode(FORMAT))
+    conn.send("ok".encode(FORMAT))
 
 def send_file(conn, name, path):
     print(f"Checking : {name}")
@@ -116,39 +151,63 @@ def send_file(conn, name, path):
     print("File sent")
     conn.send("ok".encode(FORMAT))
 
+def msg_time(ip,port,msg):
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d@%H:%M:%S")
+    m_time=print(f"[{ip}:{port}-{current_time}]: {msg}")
+    return m_time
+
 
 def handle_client(conn, addr, client_id):
-    print(f"New connection: {addr} client id: {client_id}")
+    ip, port = addr
+    conn_time=datetime.datetime.now().strftime("%Y-%m-%d@%H:%M:%S")
+    print(f"New connection: {ip}:{port}-{conn_time} client id: {client_id}")
+
     connected = True
     while connected:
+#        current_time = datetime.datetime.now().strftime("%Y-%m-%d@%H:%M:%S")
         msg = conn.recv(SIZE).decode(FORMAT)
-        print(f"Message from {addr}: {msg}")
+#        print(f"Message from {addr}: {msg}")
+#        print(f"[{ip}:{port}-{current_time}]: {msg}")
         if msg == "exit":
+            msg_time(ip,port,msg)
             print(f"Connection from client {client_id} closed ({addr})")
             conn.send("Disconnecteddddddd".encode(FORMAT))
             connected = False
         elif msg[:3] == "ls ":
-            print(f"ls {addr}")
-            ls(conn, msg[3:])
+            msg_time(ip,port,msg)
+#            print(f"ls {addr}")
+            args1 = msg.split(" ")
+            ls(conn, args1[1], args1[2])
+#            ls(conn, msg[3:])
         elif msg[0:3] == "cd ":
+            '''
             cPath=os.getcwd()
             print(cPath)
             print(f"cd {addr}")
             cd(conn,msg[3:],cPath)
+            '''
+            msg_time(ip,port,msg)
+#            print(f"cd {addr}")
+            args = msg.split(" ")
+            cd(conn, args[1], args[2])
         elif msg[0:6] == "mkdir ":
-            print(f"mkdir {addr}")
-            print(msg)
+            msg_time(ip,port,msg)
+#            print(f"mkdir {addr}")
+#            print(msg)
             mkdir(conn, msg[6:])
         elif msg[0:7] == "upload ":
-            print(f"upload {addr}")
+            msg_time(ip,port,msg)
+#            print(f"upload {addr}")
             args = msg.split(" ")
             receive_file(conn, args[1], args[2])
         elif msg[0:9] == "download ":
-            print(f"download{addr}")
+            msg_time(ip,port,msg)
+#            print(f"download{addr}")
             args = msg.split(" ")
             send_file(conn, args[1], args[2])
         else:
-            print(f"Command not found from {addr}")
+            msg_time(ip,port,msg)
+#            print(f"Command not found from {addr}")
             conn.send("Command not found".encode(FORMAT))
 
     conn.close()
@@ -156,16 +215,16 @@ def handle_client(conn, addr, client_id):
 
 def server_program():
     client_id = 0
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-
-    server.listen(10)
+    serverr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    IP,PORT= server()
+    serverr.bind((IP,PORT))
+    serverr.listen()
     print(f"Server started on {IP}:{PORT}")
     print("Waiting for connection...")
 
     while True:
-        conn, addr = server.accept()
-        print (conn , addr)
+        conn, addr = serverr.accept()
+#        print (conn , addr)
         client_id += 1
         thread = threading.Thread(target=handle_client, args=(conn, addr, client_id))
         thread.start()
